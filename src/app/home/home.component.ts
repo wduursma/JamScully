e283509c14474bfac6cffe4749842890df196ef3
Wietze Duursma <w.duursma@technischeunie.com>
2019-12-30 12:16:39 +0100

    data service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private data: DataService
+  ) { }
 
   ngOnInit() {
+  }
+  firstClick() {
+    this.data.firstClick();
   }
 
 }
